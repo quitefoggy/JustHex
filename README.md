6c2091744f266733977510c637bd48f1eda22ce1
quitefoggy <tumanoffsenya1@ukr.net>
2026-02-23 17:57:00 +0200

    First lab check

---
+++ README.md
@@ -0,0 +1,24 @@
+# JustHex
+
+This is a simple program to view and edit files in "hex" form.
+
+## Manual
+
+To use this program, simply open the "index.html" in your web browser.
+In hex collumn, you can only write the base16 symbols (0-9 and A-F), while in string collum, you can use most of your keyboard symbols
+
+
+You can change some settings like row and byte-group size in the menu at the top. 
+They will be saved to your browser's storage so you wouldn't have to edit them each time.
+
+
+# Copyright
+
+Copyright 2026 quitefoggy
+
+
+``This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.``
---
+++ files/logic_and_style/index.js
@@ -0,0 +1,160 @@
+`use strict`
+
+const saveFile = {
+    row_length:16,
+    group_size:4
+};
+
+
+
+//let a = 0;
+//let index_array = [];
+//let tile_array = [];
+//let my_tileStorage = {};
+
+const container = document.querySelector(".main_area");
+const fileInput = document.getElementById("openButton");
+const file_contents = document.getElementById("fileContent");
+
+fileInput.addEventListener("change",handleFile);
+
+function add_a_space(a_reading_point){
+    if ((a_reading_point+1)%4==0){
+        return " "
+    } else {
+        return ""
+    }
+}
+
+function correct_length(input_string){
+    if (input_string.length==1){
+        return ("0"+input_string).toUpperCase();
+    } else{
+        return (input_string).toUpperCase();
+    }
+}
+
+function outputFile(buffer){
+    const data = new DataView(buffer);
+    const iterator = {
+        idx:0,
+        next(){
+            const done = this.idx>=data.byteLength;
+            if (done){
+                return {done, value: null}
+            }
+            return{
+                done,
+                value: {
+                    idx: this.idx, 
+                    byte:correct_length(data.getUint8(this.idx++).toString(16).toUpperCase())
+                }
+            };
+        },
+        [Symbol.iterator](){
+            return this;
+        }
+    };
+
+    let row_array = [];
+    let li_array = [];
+    for (const {idx,byte} of iterator){
+        //console.log(a_part_of_data);
+        if (idx % saveFile.row_length ==0){
+            let ul = document.createElement("ul"); //рядок
+            ul.className = "file_ul";
+            row_array.push(ul);
+        };
+        if (idx % saveFile.group_size ==0){
+            let li = document.createElement("li"); //елемент рядка
+            li.className = "file_li";
+            row_array[row_array.length-1].appendChild(li);
+            li_array.push(li);
+        };
+        li_array[li_array.length-1].textContent+=byte;
+        file_contents.appendChild(row_array[row_array.length-1]);
+    }
+}
+
+function handleFile(event){
+    const file = event.target.files[0];
+    if (!file) {
+        console.log("No file selected. Please choose a file.");
+        return;
+    };
+    const reader = new FileReader();
+    reader.onload = () => {
+        //console.log("sent");
+        //console.log(reader.result.length);
+        console.log(reader.result.byteLength);
+        console.log(reader.result);
+        file_contents.textContent='';
+        outputFile(reader.result);
+        
+    };
+    reader.onerror = () => {
+        console.log("Error reading the file. Please try again.");
+    };
+    reader.readAsArrayBuffer(file);
+}
+
+
+
+/*
+function add_a_quiz(){
+    
+    if(localStorage.length===0){
+        localStorage.setItem(a, a+20);
+        let tile = document.createElement("div");
+        tile.className = "tile";
+        container.appendChild(tile);
+        index_array.push(String(String(localStorage.key(a))+" "+String(localStorage.getItem(localStorage.key(a)))));
+        tile_array.push(tile);
+        console.log(index_array);
+        //console.log(tile_array);
+        console.log(localStorage)
+        a++;
+    } else if (localStorage.length>0){
+        for (let i = 0;i<index_array.length; i++){
+            console.log(index_array)
+            console.log(localStorage)
+            let temp_array = String(String(localStorage.key(i))+" "+String(localStorage.getItem(localStorage.key(i))))
+            if (index_array.indexOf(temp_array)===-1){
+                index_array.push(temp_array);
+                console.log("check1")
+            }
+            console.log("")
+        }
+        if (!localStorage.key(a)){
+            console.log("check2")
+            localStorage.setItem(a, a+20);
+            let tile = document.createElement("div");
+            tile.className = "tile";
+            container.appendChild(tile);
+            tile_array.push(tile);
+            a++;
+        }
+        console.log(index_array);
+        console.log(localStorage)
+    };
+};
+function delete_a_quiz(){
+    if (localStorage.length>0){
+        console.log(index_array);
+        console.log(index_array[index_array.length-1]);
+        console.log(localStorage.length)
+        container.removeChild(tile_array[index_array.length-1]);
+        localStorage.removeItem(String(index_array[index_array.length-1]).split(" ")[0]);
+        console.log(localStorage)
+        tile_array.pop();
+        index_array.pop();
+        console.log("check3")
+        if (!a<=0){
+            a--;
+        }
+    }
+};
+*/
+function clear_my_localStorage(){
+    localStorage.clear();
+}
