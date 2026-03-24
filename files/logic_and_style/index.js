a702a13bdee95da56bb75e7334d36d92f9a883fb
quitefoggy <tumanoffsenya1@ukr.net>
2026-03-24 12:09:02 +0200

    pushing latest changes, might revert later...

--- files/logic_and_style/index.js
+++ files/logic_and_style/index.js
@@ -1,9 +1,6 @@
 `use strict`
 
-const saveFile = JSON.parse(localStorage.getItem("JustHex_saveFile")) || {
-    row_length:16,
-    group_size:4
-};
+
 localStorage.setItem("JustHex_saveFile",JSON.stringify(saveFile));
 
 let byte_buffer = [];
